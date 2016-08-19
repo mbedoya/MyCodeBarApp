31a6c634bf4a13e5ba1d358a4c67e6270c94babe
Mauricio <mauricio.bedoya@gmail.com>
2016-08-19 17:05:13 -0500

    PDF_417 enabled again

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,7 +38,9 @@ angular.module('starter.controllers', [])
 
       //Find The Name
       var nameIndex = bloodTypeIndex - 17;
+      console.log("Chars");
       while (isNaN(text.substr(nameIndex, 1)) || text.charCodeAt(nameIndex) == 32) {
+        console.log(text.charCodeAt(nameIndex));
         nameIndex--;
       }
       nameIndex++;
@@ -87,7 +89,7 @@ angular.module('starter.controllers', [])
           "preferFrontCamera": false, // iOS and Android
           "showFlipCameraButton": true, // iOS and Android
           "prompt": "Place a barcode inside the scan area", // supported on Android only
-          "formats": "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+          "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
         }
       );
