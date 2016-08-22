7f454a5585f178f2641dd1255df10a16318a20e3
Mauricio <mauricio.bedoya@gmail.com>
2016-08-22 16:48:32 -0500

    parser function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,24 +2,31 @@ angular.module('starter.controllers', [])
 
   .controller('DashCtrl', function ($scope) {
 
-    $scope.parse = function () {
+    $scope.parseText = function (sourceText, fx) {
 
-      var text = "";
+      //Texto legible donde se encuentra la información de la persona
+      var text;
       var endCharFound = false;
 
-      var endIndex = String($scope.model.value).indexOf("+");
+      //Buscar los últimos caracteres del texto legible, signo positivo o negativo del tipo de sangre
+      var endIndex = sourceText.indexOf("+");
       if (endIndex > -1) {
-        text = String($scope.model.value).substring(0, endIndex + 1);
+        text = sourceText.substring(0, endIndex + 1);
         endCharFound = true;
       } else {
         endIndex = String($scope.model.value).indexOf("-");
         if (endIndex > -1) {
-          text = String($scope.model.value).substring(0, endIndex + 1);
+          text = sourceText.substring(0, endIndex + 1);
           endCharFound = true;
         } else {
           console.log("No se puede procesar la cédula");
+          fx(null, "No se puede procesar la cédula");
+
+          return;
         }
       }
+
+      //Determinar si el tipo de sangre tiene 1(A) ó 2(AB) caracteres 
 
       //if only 1 char blood type
       var bloodTypeIndex = text.length - 2;
@@ -32,9 +39,8 @@ angular.module('starter.controllers', [])
 
       //Blood Type
       console.log("Blood Type:" + text.substr(bloodTypeIndex, bloodTypeChars));
-      $scope.bloodType = text.substr(bloodTypeIndex, bloodTypeChars);
-      $scope.birthDate = text.substr(bloodTypeIndex - 14, 8);
-      $scope.gender = text.substr(bloodTypeIndex - 15, 1);
+
+      //Nombre
 
       //Find The Name
       var nameIndex = bloodTypeIndex - 17;
@@ -47,19 +53,22 @@ angular.module('starter.controllers', [])
       console.log("Gender index: " + (bloodTypeIndex - 15));
       console.log("Name index: " + (nameIndex) + " " + text.charCodeAt(nameIndex));
 
+      //Con el nombre puede venir el caracter NULL o WHITE SPACE como separador, si es NULL pasar a WHITE SPACE
+
       var name = text.substr(nameIndex, bloodTypeIndex - 17 - nameIndex);
       var formattedName = '';
       var asciiName = '';
       for (var index = 0; index < name.length; index++) {
-         var char = name.charCodeAt(index); 
-        if(char == 0){
+        var char = name.charCodeAt(index);
+        if (char == 0) {
           char = 32;
         }
         asciiName = asciiName + name.charCodeAt(index) + '-';
         formattedName = formattedName + String.fromCharCode(char);
       }
-      $scope.ascii = asciiName;
-      $scope.name = formattedName;
+
+      //Obtener el ID de la Cedula, es posible que vengan 1, 2 ´ó 3 ceros dependiendo
+      //de lo largo del número
 
       var idIndex = nameIndex - 10;
       var indexChars = 10;
@@ -70,16 +79,42 @@ angular.module('starter.controllers', [])
         if (text.substr(idIndex, 1) == 0) {
           idIndex++;
           indexChars--;
+          if (text.substr(idIndex, 1) == 0) {
+            idIndex++;
+            indexChars--;
+          }
         }
       }
 
-      $scope.id = text.substr(idIndex, indexChars);
-
-      if (endCharFound) {
-        console.log($scope.model.value);
-        console.log(endIndex);
-        console.log(text);
+      var obj = {
+        name: formattedName,
+        ascii: asciiName,
+        gender: text.substr(bloodTypeIndex - 15, 1),
+        id: text.substr(idIndex, indexChars),
+        bloodType: text.substr(bloodTypeIndex, bloodTypeChars),
+        birthDate: text.substr(bloodTypeIndex - 14, 8)
       }
+
+      fx(obj);
+
+    }
+
+    $scope.parse = function () {
+
+      $scope.parseText($scope.model.value, function (obj, error) {
+
+        if (error) {
+          alert(error);
+        } else {
+          $scope.id = obj.id;
+          $scope.ascii = obj.ascii;
+          $scope.name = obj.name;
+          $scope.bloodType = obj.bloodType;
+          $scope.birthDate = obj.birthDate;
+          $scope.gender = obj.gender;
+        }
+
+      });
     }
 
     $scope.scan = function () {
